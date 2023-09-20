d0243624da6a68a7360f597f2aefc6f75c3c9286
athenacats <124017569+athenacats@users.noreply.github.com>
2023-09-20 12:14:27 +0300

    Impossible to use Card component, use div

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import { Card } from "@canonical/react-components";
 import "./App.css";
 import { useEffect, useState } from "react";
 
 interface CardData {
   title: {
+    rendered: string;
+  };
+  content: {
     rendered: string;
   };
 }
@@ -22,14 +24,27 @@ function App() {
       })
       .catch((error) => console.error("Error fetching data:", error));
   }, []);
+
+  function htmlToText(html: string): string {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(html, "text/html");
+    return doc.body.textContent || "";
+  }
+
   return (
     <>
       {cardDataArray!.map((cardData, index) => (
-        <Card
+        <div
+          className="p-card"
           key={index}
-          title={cardData?.title.rendered || "Loading..."}
+          title="CLOUD AND SERVER"
+
           //dangerouslySetInnerHTML={{ __html: cardData.content.rendered }}
-        ></Card>
+        >
+          <h3>CLOUD AND SERVER</h3>
+          <hr className="u-sv1"></hr>
+          {htmlToText(cardData.content.rendered)}
+        </div>
       ))}
     </>
   );
