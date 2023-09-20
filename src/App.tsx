c271d977d692544537275237ca3331deda3a9459
athenacats <124017569+athenacats@users.noreply.github.com>
2023-09-20 09:58:57 +0300

    Initialize project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Card } from "@canonical/react-components";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Card title="Raspberry Pi2 and Pi3">
+        For fun, for education and for profit, the RPi makes device development
+        personal and entertaining. With support for both the Pi2 and the new
+        Pi3, Ubuntu Core supports the world’s most beloved board.
+      </Card>
+    </>
+  );
+}
+
+export default App;
