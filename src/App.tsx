237b2ec303363df58f6341b5cb8d6993455361eb
athenacats <124017569+athenacats@users.noreply.github.com>
2023-09-20 10:36:45 +0300

    Pull data from api, console.log data

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,32 @@
 import { Card } from "@canonical/react-components";
 import "./App.css";
+import { useEffect, useState } from "react";
+
+interface CardData {
+  title: string;
+  imageSrc: string;
+  imageAlt: string;
+  content: string;
+}
 
 function App() {
+  const [cardData, setCardData] = useState<CardData | null>(null);
+
+  useEffect(() => {
+    // Fetch the JSON data from the external URL
+    fetch(
+      "https://people.canonical.com/~anthonydillon/wp-json/wp/v2/posts.json"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Fetched data:", data);
+        setCardData(data);
+      })
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
   return (
     <>
-      <Card title="Raspberry Pi2 and Pi3">
-        For fun, for education and for profit, the RPi makes device development
-        personal and entertaining. With support for both the Pi2 and the new
-        Pi3, Ubuntu Core supports the world’s most beloved board.
-      </Card>
+      <Card title={cardData?.title || "Loading..."}>{cardData?.content}</Card>
     </>
   );
 }
